05f3209ae791f68cbe60e01f5e4e5bf547729614
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-11-10 12:40:24 +0530

    chat

---
+++ index.js
@@ -0,0 +1,64 @@
+import '@babel/polyfill' 
+function index() {
+    //This method retrieves a list of all languages
+    async function getLanguages() {
+        let response = await fetch("/api/get-languages", {
+            method: 'GET'
+        }); 
+        return await response.json();
+    }
+
+   //This method retrieves a list of all language models
+    async function getModels() {
+        let response = await fetch("/api/get-model-list", {
+            method: 'GET'
+        })
+        return await response.json();
+    }
+}
+
+index();
+export default index;
+
+function index() {    
+    getTranslatableLangauges()
+  
+    function getTranslatableLangauges() {
+        //get languages and all language models
+        const allLanguages = getLanguages();
+        const models = getModels();
+        
+        //resolve the promises
+        Promise.all([allLanguages, models]).then( values => {
+            const allLanguages =  values[0].languages;
+            const models = values[1].models;
+            
+            //get translation models that have English as their source
+            const englishModels = models.filter(model => model.source === "en");
+            
+            //get all languages that can be translated from English
+            let translatableEnglishLanguages = englishModels.map(model => {
+                return allLanguages.find(language => model.target === language.language)
+            })
+          
+            //sort languages
+            translatableEnglishLanguages.sort((a,b) => {
+                var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                return -1;
+                }
+                if (nameA > nameB) {
+                return 1;
+                }
+            
+                // names must be equal
+                return 0;
+            })
+            const languagesMap = translatableEnglishLanguages.map( language => 
+                `<option value="${language.language}">${language.name}</option>`
+            )
+            $("#languageSelector").html(languagesMap)
+        })
+    }
+}
